97fcbcec3895586743ee8dfdd068a8386db5116d
Junwoo Shin <wnsdn020707@gmail.com>
2026-07-10 10:46:23 +0900

    fix: update register page layout

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,17 +1,60 @@
+import {
+  Container,
+  Box,
+  Card,
+  CardContent,
+  Stack,
+  TextField,
+  Button,
+  Typography,
+} from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+export default function RegisterPage() {
+  const navigate = useNavigate();
+
+  function handleLoginButtonClick() {
+    navigate("/login");
+  }
+
   return (
-      <Container maxWidth="sm">
-        <Box>
-          <Card>
-            <CardContent>
-              <Stack spacing={1}>
-                <Typography variant="h5">회원가입</Typography>
-                <TextField label="이름" variant="outlined"/>
-                <TextField label="아이디" variant="outlined"/>
-                <TextField label="비밀번호" variant="outlined" type="password"/>
-                <Button variant="contained">가입하기</Button>
-              </Stack>
-            </CardContent>
-          </Card>
-        </Box>
-      </Container>
+    <Container maxWidth="sm">
+      <Box sx={{ mt: 5 }}>
+        <Card>
+          <CardContent>
+            <Stack spacing={2}>
+              <Typography variant="h5">회원가입</Typography>
+
+              <TextField
+                label="이름"
+                variant="outlined"
+              />
+
+              <TextField
+                label="아이디"
+                variant="outlined"
+              />
+
+              <TextField
+                label="비밀번호"
+                type="password"
+                variant="outlined"
+              />
+
+              <Button variant="contained">
+                가입하기
+              </Button>
+
+              <Button
+                variant="outlined"
+                onClick={handleLoginButtonClick}
+              >
+                로그인으로 이동
+              </Button>
+            </Stack>
+          </CardContent>
+        </Card>
+      </Box>
+    </Container>
   );
+}
