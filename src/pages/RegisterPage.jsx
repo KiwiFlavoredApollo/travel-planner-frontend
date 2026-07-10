1e15c81603ed1a9b06aeff0b13fca12fac375b44
fractalbarista <fractalbarista@gmail.com>
2026-07-10 13:34:54 +0900

    axios.js 를 추가하여 백엔드 엔드포인트에 대한 객체 추가 RegisterPage에서 백엔드와 통신할 수 있도록 함수 추가

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -8,8 +8,50 @@ import {
   Button,
   Typography,
 } from "@mui/material";
+import {api} from "../api/axios.js";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 export default function RegisterPage() {
+  const [name, setName] = useState("");
+  const [userId, setUserId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  async function handleRegisterButtonClick() {
+    navigate("/login");
+
+    await api.post("/auth/register", {
+      name: name,
+      userId: userId,
+      password: password,
+    }, {})
+        .then((response) => {
+          if (response.status !== 201) {
+            return;
+          }
+
+          navigate("/login");
+
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+  }
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleUserIdChange(event) {
+    setUserId(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
   return (
       <Container
           maxWidth="xs"
@@ -29,20 +71,23 @@ export default function RegisterPage() {
               <TextField
                   label="이름"
                   variant="outlined"
+                  onChange={handleNameChange}
               />
 
               <TextField
                   label="아이디"
                   variant="outlined"
+                  onChange={handleUserIdChange}
               />
 
               <TextField
                   label="비밀번호"
                   type="password"
                   variant="outlined"
+                  onChange={handlePasswordChange}
               />
 
-              <Button variant="contained">
+              <Button variant="contained" onClick={handleRegisterButtonClick}>
                 가입하기
               </Button>
             </Stack>
